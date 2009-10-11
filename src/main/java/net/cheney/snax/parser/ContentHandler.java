417ad459e84a37c96f0eb2809a41dbf33007ea25
Dave Cheney <dave@cheney.net>
2009-10-11 23:57:49 +1100

    No need to check if the string == null, as the method is @NonNull

--- src/main/java/net/cheney/snax/parser/ContentHandler.java
+++ src/main/java/net/cheney/snax/parser/ContentHandler.java
@@ -53,11 +53,7 @@ public final class ContentHandler {
 	}
 	
     public static boolean isBlank(@Nonnull CharSequence str) {
-        int strLen;
-        if (str == null || (strLen = str.length()) == 0) {
-            return true;
-        }
-        for (int i = 0; i < strLen; i++) {
+        for (int i = 0, strLen = str.length(); i < strLen; i++) {
             if ((Character.isWhitespace(str.charAt(i)) == false)) {
                 return false;
             }
