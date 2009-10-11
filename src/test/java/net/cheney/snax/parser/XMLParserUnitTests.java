0ecf7d55f0e5cef626d8a88542ac8c5059274955
Dave Cheney <dave@cheney.net>
2009-10-11 20:59:26 +1100

    Adding unit tests to get the coverage up on the parser before I refactor where length will be incremented

--- src/test/java/net/cheney/snax/parser/XMLParserUnitTests.java
+++ src/test/java/net/cheney/snax/parser/XMLParserUnitTests.java
@@ -10,8 +10,15 @@ import org.junit.Test;
 
 public class XMLParserUnitTests {
 
+	@Test public void testEmptyElement() {
+		String xml = "<element foo='bar'/>";
+		Element expected = new Element("element", new Attribute("foo", "bar"));
+		Document doc = new XMLBuilder().parse(xml);
+		Assert.assertEquals(expected, doc.rootElement());
+	}
+	
 	@Test public void testWhitespaceOnElementEnd() {
-		String xml = "<element foo='bar' /></element>";
+		String xml = "<element foo='bar' ></element>";
 		Element expected = new Element("element", new Attribute("foo", "bar"));
 		Document doc = new XMLBuilder().parse(xml);
 		Assert.assertEquals(expected, doc.rootElement());
