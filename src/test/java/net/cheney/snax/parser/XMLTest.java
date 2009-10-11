27abcc88e83ee2984a03d30ce023b2d93721f821
Dave Cheney <dave@cheney.net>
2009-10-11 17:36:39 +1100

    Check that xml can be parsed incrementally in both constant and random chunk sizes

--- src/test/java/net/cheney/snax/parser/XMLTest.java
+++ src/test/java/net/cheney/snax/parser/XMLTest.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 
 import junit.framework.Assert;
 
@@ -11,6 +12,7 @@ import net.cheney.snax.model.Document;
 import net.cheney.snax.parser.XMLBuilder;
 
 import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang.StringUtils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
@@ -19,14 +21,43 @@ import org.junit.runners.Parameterized.Parameters;
 @RunWith(Parameterized.class)
 public class XMLTest {
 
-	private final InputStream stream;
+	private final String body;
+	private final int CHUNK_SIZE = 1024;
 
-	public XMLTest(String name) {
-		stream = XMLTest.class.getClassLoader().getResourceAsStream(name);
+	public XMLTest(String name) throws IOException {
+		InputStream stream = XMLTest.class.getClassLoader().getResourceAsStream(name);
+		body = IOUtils.toString(stream);
+		stream.close();
 	}
 	
-	@Test public void testXMLParser() throws IOException {
-		Document doc = new XMLBuilder().parse(IOUtils.toString(stream));
+	@Test public void testXMLParserWithWholeBody() {
+		Document doc = new XMLBuilder().parse(body);
+		Assert.assertNotNull(doc);
+	}
+	
+	@Test public void testXMLParserWithWholeChunk() {
+		XMLBuilder builder = new XMLBuilder();
+		String remainder = body;
+		Document doc = null;
+		while(!remainder.isEmpty()) {
+			String chunk = StringUtils.substring(remainder, 0, CHUNK_SIZE);
+			doc = builder.parse(chunk);
+			remainder = StringUtils.substring(remainder, CHUNK_SIZE);
+		}
+		Assert.assertNotNull(doc);
+	}
+	
+	@Test public void testXMLParserWithRandomChunk() {
+		XMLBuilder builder = new XMLBuilder();
+		Random r = new Random(200910110000L);
+		String remainder = body;
+		Document doc = null;
+		while(!remainder.isEmpty()) {
+			int chunkSize = r.nextInt();
+			String chunk = StringUtils.substring(remainder, 0, chunkSize);
+			doc = builder.parse(chunk);
+			remainder = StringUtils.substring(remainder, chunkSize);
+		}
 		Assert.assertNotNull(doc);
 	}
 
