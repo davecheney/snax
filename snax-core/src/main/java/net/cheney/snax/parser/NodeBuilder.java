b5dec65321bd9498a7acb3616f5ef5037b9b27e3
Dave Cheney <dave@cheney.net>
2010-03-12 15:17:52 +1100

    Reuse MutableNodeList class

--- snax-core/src/main/java/net/cheney/snax/parser/NodeBuilder.java
+++ snax-core/src/main/java/net/cheney/snax/parser/NodeBuilder.java
@@ -2,6 +2,7 @@ package net.cheney.snax.parser;
 
 import javax.annotation.Nonnull;
 
+import net.cheney.snax.builder.MutableNodeList;
 import net.cheney.snax.model.Comment;
 import net.cheney.snax.model.Namespace;
 import net.cheney.snax.model.Node;
@@ -40,58 +41,6 @@ abstract class NodeBuilder  {
 	}
 	
 	protected abstract Namespace declaredNamespaceForPrefix(String prefix);
-	
-	private static final class MutableNodeList implements java.lang.Iterable<Node> {
-
-		private Node[] elements;
-		private int length = 0;
-
-		MutableNodeList(int size) {
-			this.elements = new Node[size];
-		}
-
-		@Override
-		public Iterator iterator() {
-			return new Iterator();
-		}
-
-		private void doubleCapacity() {
-			Node[] target = new Node[elements.length * 2];
-			System.arraycopy(elements, 0, target, 0, elements.length);
-			elements = target;
-		}
-
-		public void add(Node content) {
-			ensureCapacity();
-			elements[length++] = content;
-		}
-
-		private void ensureCapacity() {
-			if(length == elements.length) {
-				doubleCapacity();
-			}
-		}
-
-		public final class Iterator implements java.util.Iterator<Node> {
-			
-			private int offset = 0;
-
-			@Override
-			public boolean hasNext() {
-				return offset < length;
-			}
-
-			@Override
-			public Node next() {
-				return elements[offset++];
-			}
-
-			@Override
-			public void remove() {
-				// yangi
-			}
-		}
-	}
 	
 	static final class NamespaceMap {
 
